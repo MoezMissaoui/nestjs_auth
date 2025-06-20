d92bac5bb80447e8a221e36cb54ac086927c0ff7
Moez Missaoui <moeiz.missaoui@gmail.com>
2025-06-20 16:21:10 +0100

    update log message to specify 'auth application'

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import { AppModule } from './app.module';
 import { log } from 'console';
 
 async function bootstrap() {
-  log('Starting NestJS application...');
-  log(`Environment: ${process.env.DB_HOST}`);
+
+  log('Starting NestJS auth application...');
+  
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
